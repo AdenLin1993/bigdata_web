af9db7d4e97c3f57580e60d0173644c90126177f
Carey Lin/WZS/Wistron <Carey_Lin@wistron.com>
2021-06-09 21:36:40 +0800

    test

--- myapp/forms.py
+++ myapp/forms.py
@@ -52,8 +52,7 @@ class EditProfileForm(FlaskForm):
                 raise ValidationError('Please use a different username.')
 
 class PostForm(FlaskForm):
-    post = TextAreaField('Say something', validators=[
-        DataRequired(), Length(min=1, max=140)])
+    post = TextAreaField('Say something', validators=[DataRequired(), Length(min=1, max=140)])
     submit = SubmitField('Submit')
 
 if __name__ == '__main__':
